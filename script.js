82da468c16d28e01f7b60dfc560a5d67541e5f36
Gwenael <gwenael.klock.sin@gmail.com>
2022-06-29 08:33:46 +0200

    [+]Partie 2

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ function comptage() {
     document.getElementById("nombreClics").textContent = nombreClics;
   }
   
+var ale1 = 94450;
+var ale2 = 11000;
+var ale3 = 1500;
+  function aleratoire(){
+    ale1 = Math.floor(Math.random() * 99999)
+    ale2 = Math.floor(Math.random() * 99999)
+    ale3 = Math.floor(Math.random() * 9999)
+    document.getElementById("al1").textContent = ale1;
+    document.getElementById("al2").textContent = ale2;
+    document.getElementById("al3").textContent = ale3;
+  }
+
+  var mail = document.getElementById("mail").value
+
+  function envoi(){
+    console.log(mail);
+    if(mail == ''){
+        console.log(mail);
+    }else if (!mail ==''){
+        document.getElementById('hidemail').style.display = 'none'; 
+    }
+
+  }
